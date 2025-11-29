87e75144d50dbbb52a136f19918b4ceaa38f1816
baobao101 <brooklyne@live.com>
2025-11-29 18:20:51 -0500

    Add upDate and unDo functions for image preview

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function upDate(previewPic) {
+
+    document.getElementById('image').style.backgroundImage = "url('" + previewPic.src + "')";
+
+    document.getElementById('image').innerHTML = previewPic.alt;
+
+
+}
+
+function unDo() {
+
+    document.getElementById('image').style.backgroundImage = "url('')";
+
+    document.getElementById("image").innerHTML = "<h3>Hover over an image below to display here.</h3>";
+
+}
+
 
