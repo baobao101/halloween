cb2032a0519431590257388f4e8292acc4b11534
baobao101 <brooklyne@live.com>
2025-11-29 20:48:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -16,11 +16,22 @@ function unDo() {
 }
 
 const button = document.getElementById('language');
-button.addEventListener('click', ()=> translate());
+button.addEventListener('click', () => translate());
+let en = true;
 function translate() {
-  const t1 = document.getElementById("translate1");
-  t1.innerHTML = "<p>Masques et rires remplissent la nuit, fantômes et glamour à la lumière de la lune.<br/>Effrayants, doux, ils tournoient avec joie, chaque pas de danse, un plaisir à entendre. 🧡👻</p>"
-  const t2 = document.getElementById('image');
-  t2.innerHTML = "<h3>Choisissez une image ci-dessous pour la voir ici.</h3>"
-}
+    const t1 = document.getElementById("translate1");
+    const t2 = document.getElementById('image');
+    if (en) {
 
+        t1.innerHTML = "<p>Masques et rires remplissent la nuit, fantômes et glamour à la lumière de la lune.<br/>Effrayants, doux, ils tournoient avec joie, chaque pas de danse, un plaisir à entendre. 🧡👻</p>"
+
+        t2.innerHTML = "<h3>Choisissez une image ci-dessous pour la voir ici.</h3>"
+    } else {
+        t1.innerHTML = "<p>Masks and laughter fill the night, ghosts and glam in moonlit light. <br />Scary, sweet, they twirl with cheer, every step a joy to hear. 🧡👻</p>"
+
+        t2.innerHTML = "<h3>Hover over an image below to preview here.</h3>"
+
+    }
+    en = !en;
+}
+console.log('hi')
