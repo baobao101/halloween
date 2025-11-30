3fc8dd8fab33d5bee4b497e45e3a5eccf6d6fe29
baobao101 <brooklyne@live.com>
2025-11-29 20:59:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ function translate() {
 
         t1.innerHTML = "<p>Masques et rires remplissent la nuit, fantômes et glamour à la lumière de la lune.<br/>Effrayants, doux, ils tournoient avec joie, chaque pas de danse, un plaisir à entendre. 🧡👻</p>"
 
-        t2.innerHTML = "<h3>Choisissez une image ci-dessous pour la voir ici.</h3>"
+        t2.innerHTML = "<h3>Cliquez sur une image ci-bas pour la voir ici.</h3>"
     } else {
         t1.innerHTML = "<p>Masks and laughter fill the night, ghosts and glam in moonlit light. <br />Scary, sweet, they twirl with cheer, every step a joy to hear. 🧡👻</p>"
 
-        t2.innerHTML = "<h3>Hover over an image below to preview here.</h3>"
+        t2.innerHTML = "<h3>Click an image below to preview here.</h3>"
 
     }
     en = !en;
